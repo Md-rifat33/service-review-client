5542007233cbb9da44e788f42a2f082a96a950e0
Md-rifat33 <ultimaterifat@yahoo.com>
2022-11-08 15:56:09 +0600

    sign up section completed

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -4,6 +4,7 @@ import About from '../../pages/About/About'
 import Blogs from '../../pages/Blogs/Blogs'
 import Home from '../../pages/Home/Home/Home'
 import Login from '../../pages/Login/Login'
+import Signup from '../../pages/Login/Signup'
 
 export const routes = createBrowserRouter([
   {
@@ -14,6 +15,7 @@ export const routes = createBrowserRouter([
       { path: '/blogs', element: <Blogs /> },
       { path: '/about', element: <About /> },
       { path: '/login', element: <Login /> },
+      { path: '/signup', element: <Signup /> },
     ],
   },
 ])
--- src/pages/Login/Signup.js
+++ src/pages/Login/Signup.js
@@ -1,10 +1,49 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
   return (
-    <div>
-      <h2 className="text-2xl">This is Signup</h2>
-    </div>
+    <form className="hero w-3/4 mx-auto rounded-lg my-6 p-32 bg-base-200">
+      <div className="hero-content w-full flex-col lg:flex-row-reverse">
+        <div className="card flex-shrink-0   shadow-2xl bg-base-100">
+          <div className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                name="email"
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                name="password"
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <h3>
+              Already have an account. Please
+              <Link to="/login" className="btn btn-link mr-6">
+                Log in
+              </Link>
+            </h3>
+            <div className="form-control mt-2">
+              <button className="btn btn-primary">Sign up</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
   )
 }
 
