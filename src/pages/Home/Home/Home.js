180abac819fa8846c3cf6abaf787c0d26cf049f2
Md-rifat33 <ultimaterifat@yahoo.com>
2022-11-09 02:09:45 +0600

    service card added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -4,6 +4,7 @@ import img1 from '../../../assets/image1.jpg'
 import img2 from '../../../assets/image2.jpg'
 import img3 from '../../../assets/image3.jpg'
 import img4 from '../../../assets/image4.jpg'
+import LimitedCard from '../../Cards/ServiceLimitedCard/LimitedCard'
 
 const Home = () => {
   const services = useLoaderData()
@@ -13,7 +14,7 @@ const Home = () => {
         <h1 className="text-3xl font-bold text-center mt-5">
           Welcome to Cobbler Shop
         </h1>
-        {/* Carousel  */}
+        {/* start:  Carousel  */}
         <div className="carousel  rounded-lg mx-auto h-96 mt-10 mb-12 ">
           <div id="slide1" className="carousel-item relative w-full">
             <img src={img1} alt="" className="w-full" />
@@ -60,6 +61,15 @@ const Home = () => {
             </div>
           </div>
         </div>
+        {/* end: carousel */}
+        <h1 className="text-4xl font-bold text-center">Our Services</h1>
+        {/* start : service section */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8 mb-9">
+          {services.map((service) => (
+            <LimitedCard key={service._id} service={service} />
+          ))}
+        </div>
+        {/* end : service section */}
       </div>
     </div>
   )
